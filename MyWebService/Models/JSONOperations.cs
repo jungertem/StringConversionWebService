fd444e9d9c43715643ec8fcbab1cd8dc93fc05eb
jungertem <jungertem@gmail.com>
2019-11-09 23:20:30 +0200

    Upload project

---
+++ MyWebService/Models/JSONOperations.cs
@@ -0,0 +1,22 @@
+﻿using System.Text.RegularExpressions;
+
+namespace MyWebService.Controllers
+{
+    public class JSONOperations
+    {
+        public string Escape(string toEscape)
+        {
+            toEscape = toEscape ?? string.Empty;
+            toEscape = Regex.Escape(toEscape);
+
+            return toEscape;
+        }
+        public string UnEscape(string toUnEscape)
+        {
+            toUnEscape = toUnEscape ?? string.Empty;
+            toUnEscape = Regex.Unescape(toUnEscape);
+
+            return toUnEscape;
+        }
+    }
+}
